fc250e18a45cc0850cac8653e069a134afad0c7f
nikhil-RGB <bleach.ichigo.anime@gmail.com>
2022-07-03 02:39:39 +0530

    Redesign tester class

--- src/util/TesterClass.java
+++ src/util/TesterClass.java
@@ -4,7 +4,14 @@ public final class TesterClass {
 
 	public static void main(String[] args) 
 	{
-		
+		String[][] table=KakuroBoard.createFilledTable("0",5,3);
+		table[0][0]="-1";
+		table[0][1]="0 23";
+		table[0][2]="0 30";
+		table[1][1]="9";
+		table[2][1]="2";
+		KakuroBoard.setInput(table);
+		KakuroBoard.main(null);
 
 	}
 
